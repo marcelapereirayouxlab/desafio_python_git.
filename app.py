6fae29f0b9324d428b6e07f261ad80e6c570e2af
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-09-03 17:52:47 -0300

    meu primeiro commit

---
+++ app.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome?' )
+idade = input('Qual é a sua idade?')
+programacao = input('Qual é a sua linguagem de programação favorita?')
+print(f"meu nome: {nome}\nminha idade:{idade}\nLinguagem favorita: {programacao}".format(nome, idade, programacao))
+
