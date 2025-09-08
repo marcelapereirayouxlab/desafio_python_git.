ae4d85f4dc6b8ee82a5951e2e8b70ed74af1594e
Youx <marcela.pereira-youxgroup@institutoyoux.org.br>
2025-09-08 13:52:41 -0300

    primeiro comentario

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 nome = input('Qual é o seu nome?' )
-idade = input('Qual é a sua idade?')
+idade = int(input('Qual é a sua idade?'))
 programacao = input('Qual é a sua linguagem de programação favorita?')
-print(f"meu nome: {nome}\nminha idade:{idade}\nLinguagem favorita: {programacao}".format(nome, idade, programacao))
-
+print(f"meu nome: {nome}\nminha idade:{idade}\nLinguagem favorita: {programacao}".format(nome, idade, programacao) )
+ano_atual = int(input('Em que ano estamos?') )
+ano_nascimento = (ano_atual-idade)
+print (f'ano atual: {ano_atual} - idade {idade} = ano nascimento: {ano_nascimento}')
